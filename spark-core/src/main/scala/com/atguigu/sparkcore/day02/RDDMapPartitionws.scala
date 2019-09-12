755aefdfbbcfaacd2d1393193839d3db752506f9
John Doe <30752740@qq.com>
2019-09-12 11:01:26 +0800

    更新

--- spark-core/src/main/scala/com/atguigu/sparkcore/day02/RDDMapPartitionws.scala
+++ spark-core/src/main/scala/com/atguigu/sparkcore/day02/RDDMapPartitionws.scala
@@ -11,7 +11,7 @@ object RDDMapPartitionws {
         val conf = new SparkConf().setAppName("Practice").setMaster("local[2]")
         val sc = new SparkContext(conf)
         val rdd1 = sc.parallelize(Array(30, 50, 70, 60, 10, 20))
-        val rdd2 = rdd1.mapPartitions(it => it.map(x => x * x))
+        val rdd2 = rdd1.mapPartitions(it => it.map(x => x * x) )
         rdd2.collect.foreach(println)
         sc.stop()
         
