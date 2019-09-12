d87f54bfa830d94639b55f1c9fac98d23926778a
John Doe <30752740@qq.com>
2019-09-12 14:35:24 +0800

    更新

--- spark-core/src/main/scala/com/atguigu/sparkcore/day02/RDDDistinct.scala
+++ spark-core/src/main/scala/com/atguigu/sparkcore/day02/RDDDistinct.scala
@@ -7,13 +7,12 @@ import org.apache.spark.{SparkConf, SparkContext}
   * Author lzc
   * Date 2019-09-12 13:51
   */
-object RDDSample {
+object RDDDistinct {
     def main(args: Array[String]): Unit = {
         val conf = new SparkConf().setAppName("Practice").setMaster("local[2]")
         val sc = new SparkContext(conf)
-        val rdd1 = sc.parallelize(Array(30, 50, 70, 60, 10, 20, 1, 3, 4, 5))
-        // 参数1: 是否放回  第二个参数: 抽样的比例
-        val rdd2: RDD[Int] = rdd1.sample(true, 0.1)
+        val rdd1 = sc.parallelize(Array(30, 50, 70, 60, 30, 70, 10))
+        val rdd2 = rdd1.distinct()
         rdd2.collect.foreach(println)
         sc.stop()
         
