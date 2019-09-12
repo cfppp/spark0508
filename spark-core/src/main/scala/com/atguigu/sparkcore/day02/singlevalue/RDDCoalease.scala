17d8530c7f56cbf05c59f831d6eeb669b2f50969
John Doe <30752740@qq.com>
2019-09-12 17:10:19 +0800

    更新

--- spark-core/src/main/scala/com/atguigu/sparkcore/day02/singlevalue/RDDCoalease.scala
+++ spark-core/src/main/scala/com/atguigu/sparkcore/day02/singlevalue/RDDCoalease.scala
@@ -10,15 +10,16 @@ object RDDCoalease {
     def main(args: Array[String]): Unit = {
         val conf = new SparkConf().setAppName("Practice").setMaster("local[2]")
         val sc = new SparkContext(conf)
-        val rdd1 = sc.parallelize(Array(30, 50, 70, 60, 30, 70, 10), 4)
+        val rdd1 = sc.parallelize(Array(1, 2, 3, 4, 5, 6), 4)
         println(rdd1.getNumPartitions)
-//        val rdd2: RDD[Int] = rdd1.coalesce(6)
-        val rdd2 = rdd1.repartition(2)
-        println(rdd2.getNumPartitions)
+        //        val rdd2: RDD[Int] = rdd1.coalesce(6)
+        val rdd2 = rdd1.repartition(5)
+        rdd2.glom().collect.foreach(x => println(x.mkString(", ")))
         sc.stop()
         
     }
 }
+
 /*
 coalesce 默认是只能减少分区, 而且这个减少不会shuffle
     减少
