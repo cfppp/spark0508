2d39b939e64d007803a100e07774b67ee72b35ee
John Doe <30752740@qq.com>
2019-09-18 15:03:49 +0800

    更新

--- spark-core-project/src/main/scala/com/atguigu/project/app/CategoryTop10App.scala
+++ spark-core-project/src/main/scala/com/atguigu/project/app/CategoryTop10App.scala
@@ -13,6 +13,7 @@ object CategoryTop10App {
     def statCategoryTop10(sc: SparkContext, userVisitActionRDD: RDD[UserVisitAction]) = {
         
         val acc: CategoryAcc = new CategoryAcc
+        
         sc.register(acc)
         // 具体的需求
         userVisitActionRDD.foreach(action => {
@@ -22,20 +23,24 @@ object CategoryTop10App {
         // ("1","click") -> 1000   ("1", "order") -> 200
         // 按照品类id分组
         val actionGoupedByCidMap: Map[String, Map[(String, String), Long]] = acc.map.groupBy(_._1._1)
-        val categoryCountInfoList: List[CategoryCountInfo] = actionGoupedByCidMap.map{
-            case (cid, map) => CategoryCountInfo(
-                cid,
-                map.getOrElse((cid, "click"), 0L),
-                map.getOrElse((cid, "order"), 0L),
-                map.getOrElse((cid, "pay"), 0L))
-        }.toList
+        val categoryCountInfoList: List[CategoryCountInfo] = actionGoupedByCidMap
+            .map {
+                case (cid, map) =>
+                    CategoryCountInfo(
+                        cid,
+                        map.getOrElse((cid, "click"), 0L),
+                        map.getOrElse((cid, "order"), 0L),
+                        map.getOrElse((cid, "pay"), 0L))
+            }
+            .toList
         
         val top10 = categoryCountInfoList
-            .sortBy(info => (info.clickCount, info.orderCount, info.payCount))(Ordering.Tuple3(Ordering.Long.reverse,Ordering.Long.reverse,Ordering.Long.reverse))
+            .sortBy(info => (info.clickCount, info.orderCount, info.payCount))(Ordering.Tuple3(Ordering.Long.reverse, Ordering.Long.reverse, Ordering.Long.reverse))
             .take(10)
         println(top10)
     }
 }
+
 /*
 1. 使用累加器计算出来 3 个指标: 品类id点击, 下单和支付数
 
