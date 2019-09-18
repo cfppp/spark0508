338d71dd51cfff3e545e0195ea6db5063e9d9808
John Doe <30752740@qq.com>
2019-09-18 15:53:40 +0800

    update

---
+++ spark-core-project/src/main/scala/com/atguigu/project/acc/CategoryAcc.scala
@@ -0,0 +1,5 @@
+package com.atguigu.project.acc
+
+class CategoryAcc {
+    
+}
---
+++ spark-core-project/src/main/scala/com/atguigu/project/app/CategorySessionTopApp.scala
@@ -0,0 +1,11 @@
+package com.atguigu.project.app
+
+/**
+  * Author lzc
+  * Date 2019-09-18 15:12
+  */
+object CategorySessionTopApp {
+    def main(args: Array[String]): Unit = {
+        
+    }
+}
